bd9de096b8b61adde799250e53d22fa6c04dd137
gjermundhp02 <gjermund.pedersen@gmail.com>
2026-05-27 21:52:24 +0200

    fix: port file name

--- nixarr/default.nix
+++ nixarr/default.nix
@@ -342,7 +342,7 @@ in {
 
             renew_port() {
               protocol="$1"
-              port_file="${cfg.stateDir}/natpmp/$protocol_port"
+              port_file="${cfg.stateDir}/natpmp/$protocol-port"
 
               result="$(${pkgs.libnatpmp}/bin/natpmpc -a 1 0 "$protocol" 60 -g ${cfg.vpn.natPmp.providerIP})"
               echo "$result"
